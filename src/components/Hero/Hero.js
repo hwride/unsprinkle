a66cea2f78d2f10b5d907a564903f4d8bc5bc05d
Hugo Wride <hugowride@gmail.com>
2022-11-27 18:26:57 +0000

    Install prettier.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,9 @@ const Hero = () => {
                   /images/hero-img@3x.jpg 3x
                   "
         />
-        <HeroImage src="/images/hero-img.jpg"/>
+        <HeroImage src="/images/hero-img.jpg" />
       </picture>
-      <Swoop src="/swoop.svg"/>
+      <Swoop src="/swoop.svg" />
     </Wrapper>
   );
 };
